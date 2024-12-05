fdb387326d6100db7ee7c34817f89ae3e961ff7b
itsjhell <>
2024-12-06 00:24:41 +0100

    - creazione della classe Checker

---
+++ src/main/java/com/group05/mycontactmanager/utilities/Checker.java
@@ -0,0 +1,25 @@
+package com.group05.mycontactmanager.utilities;
+
+import com.group05.mycontactmanager.models.PhoneNumber;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author group05
+ */
+public class Checker {
+    
+    public static boolean checkEmail(String email) {
+        return true;
+    }
+    
+    public static boolean checkNumber(PhoneNumber number) {
+        return true;
+    }
+    
+}
